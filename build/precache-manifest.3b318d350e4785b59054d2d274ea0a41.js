e9bcc636a8b079dde3b0735f23f6ffbbe7185d1f
Roche001 <rocheharold13@gmail.com>
2022-04-16 07:44:57 +0300

    update changes

---
+++ build/precache-manifest.3b318d350e4785b59054d2d274ea0a41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3977da032235cd7c3bef",
+    "url": "/static/css/main.52458f57.chunk.css"
+  },
+  {
+    "revision": "3977da032235cd7c3bef",
+    "url": "/static/js/main.3977da03.chunk.js"
+  },
+  {
+    "revision": "779f8630e00e50f25698",
+    "url": "/static/js/1.779f8630.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4114d64c86ba55a2884fe904cdd285d4",
+    "url": "/static/media/Roche.4114d64c.jpg"
+  },
+  {
+    "revision": "94d015dcfd8f2cb0ee4c775d95e97d2c",
+    "url": "/static/media/Roche2.94d015dc.png"
+  },
+  {
+    "revision": "caba78d39a7c537d7d051645bf277336",
+    "url": "/static/media/Restaurant1.caba78d3.png"
+  },
+  {
+    "revision": "645e0678936a379f1863a9f61fcc6939",
+    "url": "/index.html"
+  }
+];
