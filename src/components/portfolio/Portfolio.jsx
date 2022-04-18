52c8c05b5bddb8581949f6f996501f33c8643f40
Roche001 <rocheharold13@gmail.com>
2022-04-18 13:21:19 +0300

    update  changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -30,6 +30,30 @@ function portfolio() {
             </a>
           </div>
         </article>
+        <article className="folio-items">
+          <div className="portfolio-image">
+            <img src="Restaurant1.png" alt="kagan" />
+          </div>
+          <div className="title">Restaurant Website</div>
+          <div className="icons">
+            <a
+              href="https://github.com/Roche001"
+              className="btn-a"
+              target="blank"
+            >
+              {" "}
+              <button className="btn-a">Github</button>
+            </a>
+            <a
+              href="file:///home/roche/Desktop/Coding/First%20Project/index.html#"
+              className="btn-a"
+              target="blank"
+            >
+              {" "}
+              <button className="btn-a">Live Demo</button>
+            </a>
+          </div>
+        </article>
       </div>
     </section>
   );
