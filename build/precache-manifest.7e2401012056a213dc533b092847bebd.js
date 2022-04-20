7a04f368f2e8a009457829e17f3877db8f28a367
Roche001 <rocheharold13@gmail.com>
2022-04-20 12:36:17 +0300

    update changes

---
+++ build/precache-manifest.7e2401012056a213dc533b092847bebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff79c56e114471e4f57a",
+    "url": "/static/css/main.25e6b3cd.chunk.css"
+  },
+  {
+    "revision": "ff79c56e114471e4f57a",
+    "url": "/static/js/main.ff79c56e.chunk.js"
+  },
+  {
+    "revision": "779f8630e00e50f25698",
+    "url": "/static/js/1.779f8630.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4114d64c86ba55a2884fe904cdd285d4",
+    "url": "/static/media/Roche.4114d64c.jpg"
+  },
+  {
+    "revision": "172aa36516632fd679260e9e23c66d7b",
+    "url": "/index.html"
+  }
+];
