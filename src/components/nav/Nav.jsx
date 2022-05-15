2ffddf06aa4f2b2c2b5043dfd5a1f3f1b6b6865b
Roche001 <rocheharold13@gmail.com>
2022-05-15 21:53:02 +0300

    update changes

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -7,42 +7,44 @@ import { GoFileSubmodule } from "react-icons/go";
 import { AiFillContacts } from "react-icons/ai";
 function Nav() {
   return (
-    <nav>
-      <a href="#h">
-        {" "}
-        <h4>
-          <AiFillHome />
-        </h4>
-        <h5 className="tooltip">Home</h5>
-      </a>
-      <a href="#About">
-        <h4>
-          <AiOutlineUser />
-        </h4>
-        <h5 className="tooltip">About</h5>
-      </a>
-      <a href="#Experience">
-        {" "}
-        <h4>
-          <BsBookmarksFill />
-        </h4>
-        <h5 className="tooltip">Experience</h5>
-      </a>
-      <a href="#Portfolio">
-        <h4>
+    <div className="navbar">
+      <nav>
+        <a href="#h">
           {" "}
-          <GoFileSubmodule />{" "}
-        </h4>
-        <h5 className="tooltip"> Portfolio</h5>
-      </a>
-      <a href="#Contacts">
-        <h4>
+          <h4>
+            <AiFillHome />
+          </h4>
+          <h5 className="tooltip">Home</h5>
+        </a>
+        <a href="#About">
+          <h4>
+            <AiOutlineUser />
+          </h4>
+          <h5 className="tooltip">About</h5>
+        </a>
+        <a href="#Experience">
           {" "}
-          <AiFillContacts />
-        </h4>
-        <h5 className="tooltip">Contacts</h5>
-      </a>
-    </nav>
+          <h4>
+            <BsBookmarksFill />
+          </h4>
+          <h5 className="tooltip">Experience</h5>
+        </a>
+        <a href="#Portfolio">
+          <h4>
+            {" "}
+            <GoFileSubmodule />{" "}
+          </h4>
+          <h5 className="tooltip"> Portfolio</h5>
+        </a>
+        <a href="#Contacts">
+          <h4>
+            {" "}
+            <AiFillContacts />
+          </h4>
+          <h5 className="tooltip">Contacts</h5>
+        </a>
+      </nav>
+    </div>
   );
 }
 
