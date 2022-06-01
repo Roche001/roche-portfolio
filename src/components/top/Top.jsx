c17d6c2439adbabb480a15dbf9ccda34bacb3cd7
Roche001 <rocheharold13@gmail.com>
2022-06-01 07:12:57 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -1,58 +1,50 @@
 import React from "react";
-import "./Nav.css";
-import {
-  Navbar,
-  Container,
-  NavItem,
-  NavDropdown,
-  MenuItem,
-} from "react-bootstrap";
+import "./Top.css";
+import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
 import { AiFillHome } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
 import { BsBookmarksFill } from "react-icons/bs";
 import { GoFileSubmodule } from "react-icons/go";
 import { AiFillContacts } from "react-icons/ai";
-function Nav() {
+function Top() {
   return (
     <Navbar bg="#020c1b" variant="#020c1b">
-      <Container>
-        <div className="navbar">
-          <nav>
-            <a href="#h">
-              <h4>
-                <AiFillHome />
-              </h4>
-              <h5 className="tooltip">Home</h5>
-            </a>
-            <a href="#About">
-              <h4>
-                <AiOutlineUser />
-              </h4>
-              <h5 className="tooltip">About</h5>
-            </a>
-            <a href="#Experience">
-              <h4>
-                <BsBookmarksFill />
-              </h4>
-              <h5 className="tooltip">Experience</h5>
-            </a>
-            <a href="#Portfolio">
-              <h4>
-                <GoFileSubmodule />
-              </h4>
-              <h5 className="tooltip"> Portfolio</h5>
-            </a>
-            <a href="#Contacts">
-              <h4>
-                <AiFillContacts />
-              </h4>
-              <h5 className="tooltip">Contacts</h5>
-            </a>
-          </nav>
-        </div>
-      </Container>
+      <div className="navbar">
+        <nav>
+          <a href="#h">
+            <h4>
+              <AiFillHome />
+            </h4>
+            <h5 className="tooltip">Home</h5>
+          </a>
+          <a href="#About">
+            <h4>
+              <AiOutlineUser />
+            </h4>
+            <h5 className="tooltip">About</h5>
+          </a>
+          <a href="#Experience">
+            <h4>
+              <BsBookmarksFill />
+            </h4>
+            <h5 className="tooltip">Experience</h5>
+          </a>
+          <a href="#Portfolio">
+            <h4>
+              <GoFileSubmodule />
+            </h4>
+            <h5 className="tooltip"> Portfolio</h5>
+          </a>
+          <a href="#Contacts">
+            <h4>
+              <AiFillContacts />
+            </h4>
+            <h5 className="tooltip">Contacts</h5>
+          </a>
+        </nav>
+      </div>
     </Navbar>
   );
 }
 
-export default Nav;
+export default Top;
