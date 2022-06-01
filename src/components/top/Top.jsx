791d85a98f8a268d8ab5889bdeae98a889904e68
Roche001 <rocheharold13@gmail.com>
2022-06-01 19:06:01 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -12,40 +12,43 @@ function Top() {
       <Navbar.Brand>
         <img src="./assets/logo6.png" alt="logo" width="40px" height="40px" />
       </Navbar.Brand>
-      <div className="navbar">
-        <nav>
-          <a href="#h">
-            <h4>
-              <AiFillHome />
-            </h4>
-            <h5 className="tooltip">Home</h5>
-          </a>
-          <a href="#About">
-            <h4>
-              <AiOutlineUser />
-            </h4>
-            <h5 className="tooltip">About</h5>
-          </a>
-          <a href="#Experience">
-            <h4>
-              <BsBookmarksFill />
-            </h4>
-            <h5 className="tooltip">Experience</h5>
-          </a>
-          <a href="#Portfolio">
-            <h4>
-              <GoFileSubmodule />
-            </h4>
-            <h5 className="tooltip"> Portfolio</h5>
-          </a>
-          <a href="#Contacts">
-            <h4>
-              <AiFillContacts />
-            </h4>
-            <h5 className="tooltip">Contacts</h5>
-          </a>
-        </nav>
-      </div>
+
+      <Nav>
+        <div className="navbar">
+          <nav>
+            <Nav.Link href="#h">
+              <h4>
+                <AiFillHome />
+              </h4>
+              <h5 className="tooltip">Home</h5>
+            </Nav.Link>
+            <Nav.Link href="#About">
+              <h4>
+                <AiOutlineUser />
+              </h4>
+              <h5 className="tooltip">About</h5>
+            </Nav.Link>
+            <Nav.Link href="#Experience">
+              <h4>
+                <BsBookmarksFill />
+              </h4>
+              <h5 className="tooltip">Experience</h5>
+            </Nav.Link>
+            <Nav.Link href="#Portfolio">
+              <h4>
+                <GoFileSubmodule />
+              </h4>
+              <h5 className="tooltip"> Portfolio</h5>
+            </Nav.Link>
+            <Nav.Link href="#Contacts">
+              <h4>
+                <AiFillContacts />
+              </h4>
+              <h5 className="tooltip">Contacts</h5>
+            </Nav.Link>
+          </nav>
+        </div>
+      </Nav>
     </Navbar>
   );
 }
