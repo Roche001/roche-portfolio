ec03a2198241f548d57b13783eaab56f76970404
Roche001 <rocheharold13@gmail.com>
2022-06-03 06:20:34 +0300

    update changes

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -34,88 +34,84 @@ const Contacts = () => {
       <div className="row">
         <h2>Get In Touch</h2>
       </div>
-      <div className="container-a">
-        <div className="row">
-          <div className="col-md-6 justify-content-between align-items-stretch columns">
-            <div class="contact-options">
-              <div className="col-sm-2">
-                <div class="contact-option">
-                  <MdMail />
-                  <h4>Email</h4>
-                  <h5>rocheharold13@gmail.com</h5>
-                  <a href="mailto:rocheharold13@gmail.com" target="blank">
-                    Send a Message
-                  </a>
-                </div>
-              </div>
-              <div className="col-sm-2 ">
-                <div class="contact-option">
-                  <BsMessenger />
-                  <h4>Messenger</h4>
-                  <h5>Roche Harold</h5>
-                  <a
-                    href="https:m.me/profile.php?id=100006045915802"
-                    target="blank"
-                  >
-                    {" "}
-                    Send a Message
-                  </a>
-                </div>
-              </div>
-              <div className="col-sm-2">
-                <div class="contact-option">
-                  <BsWhatsapp />
-                  <h4>WhatsApp</h4>
-                  <a
-                    href="https://api.whatsapp.com/send?phone+254711229823"
-                    target="blank"
-                  >
-                    Send a Message
-                  </a>
-                  <br />
-                  <a
-                    href="https://api.whatsapp.com/send?phone+254718485502"
-                    target="blank"
-                  >
-                    Send a Message
-                  </a>
-                </div>
-              </div>
+
+      <div className="container-a row">
+        <div class="contact-options">
+          <div className="col-sm-2">
+            <div class="contact-option">
+              <MdMail />
+              <h4>Email</h4>
+              <h5>rocheharold13@gmail.com</h5>
+              <a href="mailto:rocheharold13@gmail.com" target="blank">
+                Send a Message
+              </a>
             </div>
           </div>
-          <div className="col-md-6 justify-content-between align-items-center">
-            <form ref={form} onSubmit={sendEmail}>
-              <div className="col-sm-2">
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Your Full Name"
-                  required
-                />
-              </div>
-              <div className="col-sm-2">
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Your Email"
-                  required
-                />
-              </div>
-              <div className="col-sm-2">
-                <textarea
-                  name="message"
-                  rows="8"
-                  placeholder="Your Message"
-                  required
-                ></textarea>
-
-                <button type="submit" class="btn">
-                  Send Message
-                </button>
-              </div>
-            </form>
+          <div className="col-sm-2 ">
+            <div class="contact-option">
+              <BsMessenger />
+              <h4>Messenger</h4>
+              <h5>Roche Harold</h5>
+              <a
+                href="https:m.me/profile.php?id=100006045915802"
+                target="blank"
+              >
+                {" "}
+                Send a Message
+              </a>
+            </div>
+          </div>
+          <div className="col-sm-2">
+            <div class="contact-option">
+              <BsWhatsapp />
+              <h4>WhatsApp</h4>
+              <a
+                href="https://api.whatsapp.com/send?phone+254711229823"
+                target="blank"
+              >
+                Send a Message
+              </a>
+              <br />
+              <a
+                href="https://api.whatsapp.com/send?phone+254718485502"
+                target="blank"
+              >
+                Send a Message
+              </a>
+            </div>
           </div>
         </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <div className="col-sm-2">
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Full Name"
+              required
+            />
+          </div>
+          <div className="col-sm-2">
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              required
+            />
+          </div>
+          <div className="col-sm-2">
+            <textarea
+              name="message"
+              rows="8"
+              placeholder="Your Message"
+              required
+            ></textarea>
+
+            <button type="submit" class="btn">
+              Send Message
+            </button>
+          </div>
+        </form>
       </div>
     </section>
   );
