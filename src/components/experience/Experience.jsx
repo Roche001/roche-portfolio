2e14e74ce5f2ca15d6da53ffd86b3a7217203d39
Roche001 <rocheharold13@gmail.com>
2022-06-04 09:58:51 +0300

    update changes

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,64 +4,63 @@ import { IoIosPricetag } from "react-icons/io";
 
 const Experience = () => {
   return (
-    <section id="Experience" className="container">
-      <div className="row">
-        <h2>Recently Used Technologies</h2>
-      </div>
-
-      <div className="skills row justify-content-center align-items-center">
-        <h3>Front-End Development</h3>
-        <div className="experience-info">
-          <div className="experience-details">
-            <div className="text-a">
-              <h4>
-                <span>
-                  <IoIosPricetag />
-                </span>
-                JavaScript
-              </h4>
+    <section id="Experience">
+      <h2>Recently Used Technologies</h2>
+      <div className="skills">
+        <div className="experience">
+          <h3>Front-End Development</h3>
+          <div className="experience-info">
+            <div className="experience-details">
+              <div className="text-a">
+                <h4>
+                  <span>
+                    <IoIosPricetag />
+                  </span>
+                  JavaScript
+                </h4>
+              </div>
             </div>
-          </div>
-          <div className="experience-details">
-            <div className="text-a">
+            <div className="experience-details">
+              <div className="text-a">
+                <h4>
+                  {" "}
+                  <span>
+                    <IoIosPricetag />
+                  </span>
+                  CSS
+                </h4>
+              </div>
+            </div>
+            <div className="experience-details">
+              <div className="text-a">
+                <h4>
+                  <span>
+                    <IoIosPricetag />
+                  </span>{" "}
+                  Bootstrap
+                </h4>
+              </div>
+            </div>
+            <div className="experience-details">
+              <div className="text-a">
+                <h4>
+                  {" "}
+                  <span>
+                    <IoIosPricetag />
+                  </span>
+                  HTML
+                </h4>
+              </div>
+            </div>
+            <div className="experience-details">
               <h4>
                 {" "}
                 <span>
                   <IoIosPricetag />
                 </span>
-                CSS
+                ReactJS
               </h4>
             </div>
-          </div>
-          <div className="experience-details">
-            <div className="text-a">
-              <h4>
-                <span>
-                  <IoIosPricetag />
-                </span>{" "}
-                Bootstrap
-              </h4>
-            </div>
-          </div>
-          <div className="experience-details">
-            <div className="text-a">
-              <h4>
-                {" "}
-                <span>
-                  <IoIosPricetag />
-                </span>
-                HTML
-              </h4>
-            </div>
-          </div>
-          <div className="experience-details">
-            <h4>
-              {" "}
-              <span>
-                <IoIosPricetag />
-              </span>
-              ReactJS
-            </h4>
           </div>
         </div>
       </div>
