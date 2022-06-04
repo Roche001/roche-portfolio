a753f19f544404bb8a3b325ada5b225ecf8cc9b4
Roche001 <rocheharold13@gmail.com>
2022-06-04 10:09:38 +0300

    update changes

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -9,61 +9,59 @@ const Experience = () => {
         <h2>Recently Used Technologies</h2>
       </div>
 
-      <div className="skills">
-        <div className="experience">
-          <h3>Front-End Development</h3>
-          <div className="experience-info row">
-            <div className="experience-details">
-              <div className="text-a">
-                <h4>
-                  <span>
-                    <IoIosPricetag />
-                  </span>
-                  JavaScript
-                </h4>
-              </div>
+      <div className="experience skills">
+        <h3>Front-End Development</h3>
+        <div className="experience-info">
+          <div className="experience-details">
+            <div className="text-a">
+              <h4>
+                <span>
+                  <IoIosPricetag />
+                </span>
+                JavaScript
+              </h4>
             </div>
-            <div className="experience-details">
-              <div className="text-a">
-                <h4>
-                  {" "}
-                  <span>
-                    <IoIosPricetag />
-                  </span>
-                  CSS
-                </h4>
-              </div>
-            </div>
-            <div className="experience-details">
-              <div className="text-a">
-                <h4>
-                  <span>
-                    <IoIosPricetag />
-                  </span>{" "}
-                  Bootstrap
-                </h4>
-              </div>
-            </div>
-            <div className="experience-details">
-              <div className="text-a">
-                <h4>
-                  {" "}
-                  <span>
-                    <IoIosPricetag />
-                  </span>
-                  HTML
-                </h4>
-              </div>
-            </div>
-            <div className="experience-details">
+          </div>
+          <div className="experience-details">
+            <div className="text-a">
               <h4>
                 {" "}
                 <span>
                   <IoIosPricetag />
                 </span>
-                ReactJS
+                CSS
               </h4>
             </div>
+          </div>
+          <div className="experience-details">
+            <div className="text-a">
+              <h4>
+                <span>
+                  <IoIosPricetag />
+                </span>{" "}
+                Bootstrap
+              </h4>
+            </div>
+          </div>
+          <div className="experience-details">
+            <div className="text-a">
+              <h4>
+                {" "}
+                <span>
+                  <IoIosPricetag />
+                </span>
+                HTML
+              </h4>
+            </div>
+          </div>
+          <div className="experience-details">
+            <h4>
+              {" "}
+              <span>
+                <IoIosPricetag />
+              </span>
+              ReactJS
+            </h4>
           </div>
         </div>
       </div>
