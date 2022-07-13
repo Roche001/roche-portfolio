ec1c7fef03ae9440b14ee29d075f7283b78e6b4f
Roche Harold <rocheharold13@gmail.com>
2022-07-13 22:36:27 +0300

    update changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./Header.css";
 import Jr from "./Jr";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Header = () => {
+  AOS.init({
+    duration: 2000,
+  });
   return (
     <div id="Header">
       <header className="main container-fluid">
         {" "}
         <div className="header_container">
-          <h4>Hello, my name is</h4>
-          <h1>Roche Harold</h1>
-          <h3>I am a Front-End Developer</h3>
+          <h4 data-aos="fade-right">Hello, my name is</h4>
+          <h1 data-aos="fade-left">Roche Harold</h1>
+          <h3 data-aos="fade-right">I am a Front-End Developer</h3>
           <Jr />
         </div>
       </header>
