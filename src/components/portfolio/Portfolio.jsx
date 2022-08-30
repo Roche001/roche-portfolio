325100d48d2b57a162130adbd5fadac1a4a578fa
Roche Harold <rocheharold13@gmail.com>
2022-08-30 20:38:19 +0300

    update changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,106 +1,97 @@
 import React from "react";
 import "./Portfolio.css";
+import { AiFillGithub } from "react-icons/ai";
+import { FaEye } from "react-icons/fa";
 
 function portfolio() {
   return (
-    <section id="Portfolio" className="container-fluid">
-      <div className=" port row text-center">
-        <h2>My Noteworthy Projects</h2>
-      </div>
-
-      <div className="porfolio row">
-        <article className="folio-items col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-12">
-          
-            <img
-              className="d-block w-100"
-              src="./assets/Restaurant1.png"
-              alt="kagan"
-            />
-         
-          <div className="title">Restaurant Website</div>
-          <div className="icons">
+    <div id="Portfolio" className="riley">
+      <h2>Portfolio</h2>
+      <div className="big-riley row">
+        <div className="riley-card col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+          <div className="riley-card-box">
+            <img className="d-block w-100" src="./assets/long.png" alt="hill" />
+          </div>
+          <div className="riley-text">
             <a
-              href="https://github.com/Roche001/Rhino-Hill-Restaurant.git"
-              className="btn-a"
+              href="https://github.com/Roche001/Rhino-Hill-Restaurant"
               target="_blank"
               rel="noopener noreferrer"
             >
-              {" "}
-              <button className="btn-a">Github</button>
+              <h3>
+                <AiFillGithub />
+              </h3>
             </a>
+
             <a
               href="https://rhinohillrestaurant.netlify.app/"
-              className="btn-a"
               target="_blank"
               rel="noopener noreferrer"
             >
-              {" "}
-              <button className="btn-a">Live Demo</button>
+              <h3>
+                <FaEye />
+              </h3>
             </a>
           </div>
-        </article>
-        <article className="folio-items col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-12">
-          
-            <img
-              className="d-block w-100"
-              src="./assets/Real.png"
-              alt="kagan"
-            />
-        
-          <div className="title">Real-Estate Website</div>
-          <div className="icons">
-            <a
-              href="https://github.com/Roche001/Real-Estate-website.git"
-              className="btn-a"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              <button className="btn-a">Github</button>
-            </a>
-            <a
-              href="https://pharolix.netlify.app"
-              className="btn-a"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {" "}
-              <button className="btn-a">Live Demo</button>
-            </a>
-          </div>
-        </article>
-        <article className="folio-items col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-12">
-         
+        </div>
+        <div className="riley-card col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+          <div className="riley-card-box">
             <img
               className="d-block w-100"
               src="./assets/logistics.png"
-              alt="kagan"
+              alt="hill"
             />
-         
-          <div className="title">Real-Estate Website</div>
-          <div className="icons">
+          </div>
+          <div className="riley-text">
             <a
-              href="https://github.com/Roche001/Logistics-company.git"
-              className="btn-a"
+              href="https://github.com/Roche001/Logistics-company"
               target="_blank"
               rel="noopener noreferrer"
             >
-              {" "}
-              <button className="btn-a">Github</button>
+              <h3>
+                <AiFillGithub />
+              </h3>
             </a>
+
             <a
               href="https://andelleslogistics.netlify.app/"
-              className="btn-a"
               target="_blank"
               rel="noopener noreferrer"
             >
-              {" "}
-              <button className="btn-a">Live Demo</button>
+              <h3>
+                <FaEye />
+              </h3>
             </a>
           </div>
-        </article>
+        </div>
+        <div className="riley-card col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
+          <div className="riley-card-box">
+            <img className="d-block w-100" src="./assets/rise.png" alt="hill" />
+          </div>
+          <div className="riley-text">
+            <a
+              href="https://github.com/Roche001/Rhino-Hill-Restaurant"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>
+                <AiFillGithub />
+              </h3>
+            </a>
+
+            <a
+              href="https://rhinohillrestaurant.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>
+                <FaEye />
+              </h3>
+            </a>
+          </div>
+        </div>
       </div>
-    </section>
+    </div>
   );
 }
 
