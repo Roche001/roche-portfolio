672959414de3acce64f9141c30364a0169834130
Roche Harold <rocheharold13@gmail.com>
2022-08-30 20:47:13 +0300

    update changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -70,7 +70,7 @@ function portfolio() {
           </div>
           <div className="riley-text">
             <a
-              href="https://github.com/Roche001/Rhino-Hill-Restaurant"
+              href="https://github.com/Roche001/high-rise"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -80,7 +80,7 @@ function portfolio() {
             </a>
 
             <a
-              href="https://rhinohillrestaurant.netlify.app/"
+              href="https://rhinohillrestaurant.lify.app/"
               target="_blank"
               rel="noopener noreferrer"
             >
