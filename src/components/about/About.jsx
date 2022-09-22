6c57d5202d7744500e2adf3d09a25513b29b3794
Roche Harold <rocheharold13@gmail.com>
2022-09-22 09:36:49 +0300

    update changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,11 +6,8 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 const About = () => {
-  AOS.init({
-    duration: 2000,
-  });
   return (
-    <section id="About" className="container" data-aos="zoom-in-down">
+    <section id="About" className="container">
       <div className="row text-center">
         <h2>About Me</h2>
       </div>
