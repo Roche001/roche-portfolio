8fdb0c3cf916177889e1c1b185b3ce3bb9372eb7
Roche Harold <rocheharold13@gmail.com>
2022-09-29 21:15:04 +0300

    update changes

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,73 +1,74 @@
 import React from "react";
 import "./Experience.css";
-import { IoIosPricetag } from "react-icons/io";
+import { BsBraces } from "react-icons/bs";
 
 const Experience = () => {
   return (
     <section id="Experience" className="container-fluid">
       <div className=" tech row">
-        <h2>Recently Used Technologies</h2>
+        <h2>Skills</h2>
       </div>
-
-      <div className="experience-info skills row ">
-        <div className="dev">
-          <h3>Front-End Development</h3>
+      <div className="front-container">
+        <h3>
+          <BsBraces />
+          <span>Frontend Development</span>
+        </h3>
+      </div>
+      <div className="front-end row">
+        <div className="front-end-card  ">
+          <h3>
+            <img
+              className="d-block w-100"
+              src="./assets/icon1.png"
+              alt="html"
+            />
+          </h3>
+          <h4> HTML</h4>
         </div>
-        <ul>
-          <li>
-            <div className="text-a">
-              <h4>
-                <span>
-                  <IoIosPricetag />
-                </span>
-                JavaScript
-              </h4>
-            </div>
-          </li>
-          <li>
-            <div className="text-a">
-              <h4>
-                <span>
-                  <IoIosPricetag />
-                </span>{" "}
-                Bootstrap
-              </h4>
-            </div>
-          </li>
-          <li>
-            <div className="text-a">
-              <h4>
-                {" "}
-                <span>
-                  <IoIosPricetag />
-                </span>
-                ReactJS
-              </h4>
-            </div>
-          </li>
-          <li>
-            <div className="text-a">
-              <h4>
-                {" "}
-                <span>
-                  <IoIosPricetag />
-                </span>
-                HTML
-              </h4>
-            </div>
-          </li>
-          <li>
-            <div className="text-a">
-              <h4>
-                {" "}
-                <span>
-                  <IoIosPricetag />
-                </span>
-                CSS
-              </h4>
-            </div>
-          </li>
-        </ul>
+        <div className="front-end-card ">
+          <h3>
+            <img className="d-block w-100" src="./assets/icon2.png" alt="css" />
+          </h3>
+          <h4> CSS</h4>
+        </div>
+        <div className="front-end-card">
+          <h3>
+            <img className="d-block w-100" src="./assets/icon3.png" alt="js" />
+          </h3>
+          <h4> Javascript</h4>
+        </div>
+      </div>
+      <div className="front-end row">
+        <div className="front-end-card">
+          <h3>
+            <img
+              className="d-block w-100"
+              src="./assets/icon4.png"
+              alt="React"
+            />
+          </h3>
+          <h4> React</h4>
+        </div>
+        <div className="front-end-card">
+          <h3>
+            <img
+              className="d-block w-100"
+              src="./assets/icon55.png"
+              alt="git"
+            />
+          </h3>
+          <h4> Git</h4>
+        </div>
+        <div className="front-end-card">
+          <h3>
+            <img
+              className="d-block w-100"
+              src="./assets/icon7.png"
+              alt="html"
+            />
+          </h3>
+          <h4> Bootstrap</h4>
+        </div>
       </div>
     </section>
   );
