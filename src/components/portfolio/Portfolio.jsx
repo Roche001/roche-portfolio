370dff4ea7599ef4ebce4d973c1802602a57a085
Roche Harold <rocheharold13@gmail.com>
2022-09-29 21:51:16 +0300

    update changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,84 +6,106 @@ import { IoIosShareAlt } from "react-icons/io";
 
 function portfolio() {
   return (
-    <div id="Portfolio" className="riley">
+    <div id="Portfolio" className="riley container-fluid">
       <h2>Portfolio</h2>
 
-      <div className="card-container">
-        <Card style={{ width: "18rem" }} className="card-one">
-          <Card.Img
-            variant="top"
-            src="./assets/long1(1).png"
-            alt="Rhino Hill"
-          />
-          <Card.Body>
-            <div className="card-body">
+      <div className="card-container row">
+        <div className="ranch-card nopadding col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div className="card-img">
+            <img
+              className="d-block w-100"
+              src="./assets/long1(1).png"
+              alt="Rhino Hill"
+            />
+          </div>
+          <div className="card-text">
+            <h4>Restaurant Website</h4>
+            <h4>
               <a
                 href="https://github.com/Roche001/Rhino-Hill-Restaurant.git"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 {" "}
-                <AiFillGithub />
+                <AiFillGithub /> Github
               </a>
+            </h4>
+            <h4>
+              {" "}
               <a
                 href="https://rhinohillrestaurant.netlify.app/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <IoIosShareAlt />
+                <IoIosShareAlt /> Live Demo
               </a>
-            </div>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }} className="card-one">
-          <Card.Img
-            variant="top"
-            src="./assets/logistics(1).png"
-            alt="Rhino Hill"
-          />
-          <Card.Body>
-            <div className="card-body">
+            </h4>
+          </div>
+        </div>
+        <div className="ranch-card nopadding col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div className="card-img">
+            <img
+              className="d-block w-100"
+              src="./assets/logistics(1).png"
+              alt="Rhino Hill"
+            />
+          </div>
+          <div className="card-text">
+            <h4>Logistics Website</h4>
+            <h4>
               <a
                 href="https://github.com/Roche001/Logistics-company.git"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 {" "}
-                <AiFillGithub />
+                <AiFillGithub /> Github
               </a>
+            </h4>
+            <h4>
+              {" "}
               <a
                 href="https://andelleslogistics.netlify.app/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <IoIosShareAlt />
+                <IoIosShareAlt /> Live Demo
               </a>
-            </div>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }} className="card-one">
-          <Card.Img variant="top" src="./assets/rise.png" alt="Rhino Hill" />
-          <Card.Body>
-            <div className="card-body">
+            </h4>
+          </div>
+        </div>
+        <div className="ranch-card nopadding col-xl-3 col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div className="card-img">
+            <img
+              className="d-block w-100"
+              src="./assets/rise.png"
+              alt="Rhino Hill"
+            />
+          </div>
+          <div className="card-text">
+            <h4>Real-Estate Website</h4>
+            <h4>
               <a
                 href="https://github.com/Roche001/high-rise.git"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 {" "}
-                <AiFillGithub />
+                <AiFillGithub /> Github
               </a>
+            </h4>
+            <h4>
+              {" "}
               <a
                 href="https://andelleslogistics.netlify.app/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <IoIosShareAlt />
+                <IoIosShareAlt /> Live Demo
               </a>
-            </div>
-          </Card.Body>
-        </Card>
+            </h4>
+          </div>
+        </div>
       </div>
     </div>
   );
