e9a5b08ba7fd05532c87b99833fa9d70d593e42a
Roche Harold <rocheharold13@gmail.com>
2022-11-15 18:10:03 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import "./Top.css";
 import { Nav, Navbar } from "react-bootstrap";
 import { Container } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+
 function Top() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="#020c1b" variant="dark">
-      <Container>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="#020c1b"
+      variant="dark"
+      className="navbar"
+    >
+      <Container fluid>
         <Navbar.Brand href="#Header">
           <div className="img">
             <img
@@ -16,28 +24,30 @@ function Top() {
             />
           </div>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <div className="nav-items">
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#Header">
-                <h6>Home</h6>
-              </Nav.Link>
-              <Nav.Link href="#About">
-                <h6>About</h6>
-              </Nav.Link>
-              <Nav.Link href="#Experience">
-                <h6>Skills</h6>
-              </Nav.Link>
-              <Nav.Link href="#Portfolio">
-                <h6>Portfolio</h6>
-              </Nav.Link>
-              <Nav.Link href="#Contacts">
-                <h6>Contacts</h6>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Form className="d-flex">
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#Header">
+                  <h6>Home</h6>
+                </Nav.Link>
+                <Nav.Link href="#About">
+                  <h6>About</h6>
+                </Nav.Link>
+                <Nav.Link href="#Experience">
+                  <h6>Skills</h6>
+                </Nav.Link>
+                <Nav.Link href="#Portfolio">
+                  <h6>Portfolio</h6>
+                </Nav.Link>
+                <Nav.Link href="#Contacts">
+                  <h6>Contacts</h6>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Form>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
