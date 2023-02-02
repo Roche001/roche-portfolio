3c1e8e28f45ba819e5f9bf9fbd461bb78ff6bca9
Roche Harold <rocheharold13@gmail.com>
2023-02-02 18:21:34 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Top.css";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { bar } from "../../nav";
 
 function Top() {
@@ -7,6 +8,11 @@ function Top() {
     <div className="navbar">
       <div className="logo">
         <img src="././assets/touch.png" alt="logo" />
+      </div>
+      <div className="top-icon">
+        <h4>
+          <GiHamburgerMenu />
+        </h4>
       </div>
       <div className="nav-elements">
         {bar.map((bar, index) => {
