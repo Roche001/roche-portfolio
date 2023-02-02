f0f278591cac0ac040c7ba8d44f98962a176a820
Roche Harold <rocheharold13@gmail.com>
2023-02-02 17:59:57 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -1,55 +1,23 @@
 import React from "react";
 import "./Top.css";
-import { Nav, Navbar } from "react-bootstrap";
-import { Container } from "react-bootstrap";
-import Form from "react-bootstrap/Form";
+import { bar } from "../../nav";
 
 function Top() {
   return (
-    <Navbar
-      collapseOnSelect
-      expand="lg"
-      bg="#020c1b"
-      variant="dark"
-      className="navbar"
-    >
-      <Container fluid>
-        <Navbar.Brand href="#Header">
-          <div className="img">
-            <img
-              src="./assets/touch.png"
-              alt="logo"
-              width="50px"
-              height="50px"
-            />
-          </div>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll" className="nav">
-          <Form className="d-flex">
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#Header">
-                  <h6>Home</h6>
-                </Nav.Link>
-                <Nav.Link href="#About">
-                  <h6>About</h6>
-                </Nav.Link>
-                <Nav.Link href="#Experience">
-                  <h6>Skills</h6>
-                </Nav.Link>
-                <Nav.Link href="#Portfolio">
-                  <h6>Portfolio</h6>
-                </Nav.Link>
-                <Nav.Link href="#Contacts">
-                  <h6>Contacts</h6>
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="navbar">
+      <div className="logo">
+        <img src="././assets/touch.png" alt="logo" />
+      </div>
+      <div className="nav-elements">
+        {bar.map((bar, index) => {
+          return (
+            <div key={index} className="menu-one" href={bar.link}>
+              <h6>{bar.name}</h6>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
---
+++ src/nav.js
@@ -0,0 +1,7 @@
+export const bar = [
+  { name: "Home", link: "#Header" },
+  { name: "About", link: "#About" },
+  { name: "Skills", link: "#Skills" },
+  { name: "Portfolio", link: "#Portfolio" },
+  { name: "Contacts", link: "#Contacts" },
+];
