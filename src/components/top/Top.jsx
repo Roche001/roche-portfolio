76c09cd936b721072750305e7d1a6f945ff31275
Roche Harold <rocheharold13@gmail.com>
2023-02-03 05:44:06 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Top.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { bar } from "../../nav";
 
-function Top() {
+const Top = () => {
+  const [navbarColor, setNavbarColor] = useState("transparent");
+  useEffect(() => {
+    function handleScroll() {
+      if (window.pageYOffset >= 100) {
+        setNavbarColor("#37474f");
+      } else {
+        setNavbarColor("transparent");
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ backgroundColor: navbarColor }}>
       <div className="logo">
+        {" "}
         <img src="././assets/touch.png" alt="logo" />
       </div>
       <div className="top-icon">
@@ -17,14 +33,16 @@ function Top() {
       <div className="nav-elements">
         {bar.map((bar, index) => {
           return (
-            <div key={index} className="menu-one" href={bar.link}>
-              <h6>{bar.name}</h6>
+            <div key={index} className="menu-one">
+              <a href={bar.link}>
+                <h6>{bar.name}</h6>
+              </a>
             </div>
           );
         })}
       </div>
     </div>
   );
-}
+};
 
 export default Top;
--- src/nav.js
+++ src/nav.js
@@ -1,7 +1,7 @@
 export const bar = [
   { name: "Home", link: "#Header" },
   { name: "About", link: "#About" },
-  { name: "Skills", link: "#Skills" },
+  { name: "Skills", link: "#Experience" },
   { name: "Portfolio", link: "#Portfolio" },
   { name: "Contacts", link: "#Contacts" },
 ];
