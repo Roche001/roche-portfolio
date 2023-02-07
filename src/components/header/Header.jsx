1398ed8342e4cf38c264c2364fa1ba5fce9b3660
Roche Harold <rocheharold13@gmail.com>
2023-02-07 22:54:46 +0300

    update changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,14 @@ import "./Header.css";
 import Jr from "./Jr";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const Header = () => {
   AOS.init({
     duration: 2000,
   });
   return (
-    <div id="Header">
+    <div id="Header" className="primary">
       <header className="main container-fluid">
         {" "}
         <div className="header_container">
@@ -17,6 +18,13 @@ const Header = () => {
           <h1 data-aos="fade-left">Roche Harold</h1>
           <h3 data-aos="fade-right">I am a Front-End Developer</h3>
           <Jr />
+          <a href="#About">
+            <div className="more">
+              <h4>
+                <MdKeyboardArrowDown />
+              </h4>
+            </div>
+          </a>
         </div>
       </header>
     </div>
