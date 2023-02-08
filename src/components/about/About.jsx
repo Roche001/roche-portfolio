4ad81acd138b3ee9bd4ffa335f51f8dcf1f0bfaf
Roche Harold <rocheharold13@gmail.com>
2023-02-08 15:21:53 +0300

    update changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
       </div>
 
       <div className="about row">
-        <div className="box col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+        <div className="box color col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
           <img className="d-block w-100" src="./assets/Roche2.jpg" alt="Me" />
         </div>
         <div className="text col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
